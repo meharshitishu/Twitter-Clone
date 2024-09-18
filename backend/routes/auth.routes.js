9bb1043b52b7fc822a755a7635f643e715b91c38
Harshit Sharma <sharmaharshit0808@gmail.com>
2024-09-18 15:49:33 +0530

    2- Auth Routes and Controllers function

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {login, logout, signup} from "../controller/auth.controller.js"
+import {getMe, login, logout, signup} from "../controller/auth.controller.js"
+import { protectRoute } from "../middleware/protectRoute.js";
 const router=express.Router();
 
+router.get("/me",protectRoute,getMe);
 router.post("/signup",signup);
-
 router.post("/login",login);
-
 router.post("/logout",logout);
 
 export default router;
